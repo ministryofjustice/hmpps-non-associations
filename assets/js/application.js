a7f3dcc29a74f8362a216cbd5635ec7ccd139447
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-06-13 17:57:16 +0100

    Make DPS cards/tiles clickable anywhere

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,2 +1,18 @@
 window.GOVUKFrontend.initAll()
 window.MOJFrontend.initAll()
+
+$(function pageLoaded() {
+  // for clickable cards, forward a click anywhere inside it to single contained link, if it exists
+  $('.dps-card--clickable').each((index, card) => {
+    const $links = $('.dps-card__link', card)
+    if ($links.length === 1) {
+      const $card = $(card)
+      $card.on('click', e => {
+        if (e.target.nodeName !== 'A') {
+          e.stopPropagation()
+          $links[0].click()
+        }
+      })
+    }
+  })
+})
