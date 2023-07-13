a7a3c1c836855db7e62470a37f0030ef25ea50ac
ushkarev <ushkarev@users.noreply.github.com>
2023-07-13 13:57:14 +0100

    NON-108: Add skeleton routes  (#29)
    Add skeleton routes to service non-association adding and viewing user journeys

---
+++ server/routes/add.ts
@@ -0,0 +1,16 @@
+import { type RequestHandler, Router } from 'express'
+
+import asyncMiddleware from '../middleware/asyncMiddleware'
+import type { Services } from '../services'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function addRoutes(service: Services): Router {
+  const router = Router()
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+
+  get('/', (req, res) => {
+    res.render('pages/add.njk')
+  })
+
+  return router
+}
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,15 +2,23 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
+import prisonerSearchRoutes from './prisonerSearch'
+import addRoutes from './add'
+import viewRoutes from './view'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes(service: Services): Router {
+export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res, next) => {
     res.render('pages/index')
   })
+
+  router.use('/prisoner/:prisonerNumber/non-associations/add/search-prisoner', prisonerSearchRoutes(services))
+
+  router.use('/prisoner/:prisonerNumber/non-associations/add/with-prisoner/:otherPrisonerNumber', addRoutes(services))
+
+  router.use('/prisoner/:prisonerNumber/non-associations', viewRoutes(services))
 
   return router
 }
