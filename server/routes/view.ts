a83ff572ded044e4b52196cf041c5dc6b4748636
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-07-13 12:31:17 +0100

    Add skeleton routes to service non-association adding and viewing user journeys

---
+++ server/routes/view.ts
@@ -0,0 +1,18 @@
+import { type RequestHandler, Router } from 'express'
+
+import asyncMiddleware from '../middleware/asyncMiddleware'
+import type { Services } from '../services'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function viewRoutes(service: Services): Router {
+  const router = Router()
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+
+  get('/', (req, res) => {
+    res.render('pages/view.njk', {
+      prisonerName: 'David Jones',
+    })
+  })
+
+  return router
+}
