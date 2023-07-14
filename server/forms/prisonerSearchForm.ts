6d90317af602498001bb9e6069b92665c9f2d4ef
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-07-14 12:48:13 +0100

    Create form class to handle searching for prisoners

---
+++ server/forms/prisonerSearchForm.ts
@@ -0,0 +1,22 @@
+import { BaseForm } from './index'
+
+export type PrisonerSearchData = {
+  q: string
+  page: number
+}
+
+export default class PrisonerSearchForm extends BaseForm<PrisonerSearchData> {
+  protected validate(): void {
+    if (!this.data.q || /^\s*$/.test(this.data.q)) {
+      this.addError('q', 'Enter a name or prisoner number')
+    } else {
+      this.data.q = this.data.q.trim()
+    }
+
+    this.data.page = parseInt(this.data.page as unknown as string, 10)
+    if (Number.isNaN(this.data.page) || this.data.page < 1) {
+      this.addError('page', 'Invalid page number')
+      delete this.data.page
+    }
+  }
+}
