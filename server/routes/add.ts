68ea6d0c4b1576c17d0b6a36789134af85d16353
ushkarev <ushkarev@users.noreply.github.com>
2023-07-18 16:29:47 +0100

    Ensure that all route parameters are provided to request handlers (#38)

--- server/routes/add.ts
+++ server/routes/add.ts
@@ -5,7 +5,7 @@ import type { Services } from '../services'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function addRoutes(service: Services): Router {
-  const router = Router()
+  const router = Router({ mergeParams: true })
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res) => {
