21596a7a3bdb116bec61ba9b25037312872ec3c4
Ashley Gyngell <96007411+ashleygyngell@users.noreply.github.com>
2023-07-21 10:40:38 +0100

    NON-104: Added table content to `view.ts`, headers to follow. (#46)
    * NON-104: Added table content to `view.ts`, headers to follow.
    
    * NON-104: Headers added.
    
    * NON-104: Indentation fixed
    
    * NON-104: Nudge to run CircleCI

--- server/routes/view.ts
+++ server/routes/view.ts
@@ -2,15 +2,27 @@ import { type RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function viewRoutes(service: Services): Router {
   const router = Router({ mergeParams: true })
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res) => {
+    const response = [
+      {
+        otherPrisonerName: 'Broadstairs, Liam',
+        otherPrisonerNumber: 'A8469DY',
+        reason: 'Bullying',
+        role: 'Perpetrator',
+        whereToKeepApart: 'Cell',
+        comments:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus.',
+        dateAdded: '24 May 2023 by Mary Smith',
+      },
+    ]
     res.render('pages/view.njk', {
       prisonerName: 'David Jones',
+      nonAssociations: response,
     })
   })
 
