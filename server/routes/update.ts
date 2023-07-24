a7c37ba592c9ad26e84e30cc467805606e257432
ushkarev <ushkarev@users.noreply.github.com>
2023-07-24 12:42:45 +0100

    Add close and update placeholder routes (#52)

---
+++ server/routes/update.ts
@@ -0,0 +1,37 @@
+import { type RequestHandler, Router } from 'express'
+
+import { nameOfPrisoner, reversedNameOfPrisoner } from '../utils/utils'
+import asyncMiddleware from '../middleware/asyncMiddleware'
+import HmppsAuthClient from '../data/hmppsAuthClient'
+import { OffenderSearchClient } from '../data/offenderSearch'
+import { createRedisClient } from '../data/redisClient'
+import TokenStore from '../data/tokenStore'
+import type { Services } from '../services'
+
+const hmppsAuthClient = new HmppsAuthClient(new TokenStore(createRedisClient()))
+
+export default function addRoutes(service: Services): Router {
+  const router = Router({ mergeParams: true })
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+
+  get('/', async (req, res) => {
+    const { prisonerNumber, nonAssociationId: nonAssociationIdStr } = req.params
+    const nonAssociationId = parseInt(nonAssociationIdStr, 10)
+
+    const systemToken = await hmppsAuthClient.getSystemClientToken(res.locals.user.username)
+    const offenderSearchClient = new OffenderSearchClient(systemToken)
+    const prisoner = await offenderSearchClient.getPrisoner(prisonerNumber)
+
+    res.locals.breadcrumbs.addItems(
+      { text: reversedNameOfPrisoner(prisoner), href: `${res.app.locals.dpsUrl}/prisoner/${prisonerNumber}` },
+      { text: 'Non-associations', href: service.routeUrls.view(prisonerNumber) },
+    )
+    res.render('pages/update.njk', {
+      prisonerNumber,
+      prisonerName: nameOfPrisoner(prisoner),
+      nonAssociationId,
+    })
+  })
+
+  return router
+}
