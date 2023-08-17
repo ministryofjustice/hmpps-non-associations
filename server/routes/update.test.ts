7bf42f46ef42124dd34b40e152709529435f74f5
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-08-17 17:43:48 +0100

    NON-145: Added test for loading non-association data on update

--- server/routes/update.test.ts
+++ server/routes/update.test.ts
@@ -5,9 +5,10 @@ import { SanitisedError } from '../sanitisedError'
 import { appWithAllRoutes } from './testutils/appSetup'
 import routeUrls from '../services/routeUrls'
 import { NonAssociationsApi } from '../data/nonAssociationsApi'
-import { OffenderSearchClient } from '../data/offenderSearch'
+import { OffenderSearchClient, OffenderSearchResult } from '../data/offenderSearch'
 import { mockNonAssociation } from '../data/testData/nonAssociationsApi'
 import { davidJones, fredMills } from '../data/testData/offenderSearch'
+import { nameOfPerson } from '../utils/utils'
 
 jest.mock('../data/hmppsAuthClient')
 jest.mock('../data/nonAssociationsApi')
@@ -15,6 +16,10 @@ jest.mock('../data/offenderSearch')
 
 // mock "key" prisoner
 const { prisonerNumber } = davidJones
+const prisoner = davidJones
+
+// mock "other" prisoner
+const otherPrisoner = fredMills
 
 // mock non-association
 const nonAssociation = mockNonAssociation(davidJones.prisonerNumber, fredMills.prisonerNumber)
@@ -22,13 +27,13 @@ const nonAssociationId = nonAssociation.id
 
 let app: Express
 let offenderSearchClient: jest.Mocked<OffenderSearchClient>
-let nonAssociationApi: jest.Mocked<NonAssociationsApi>
+let nonAssociationsApi: jest.Mocked<NonAssociationsApi>
 
 beforeEach(() => {
   app = appWithAllRoutes({})
 
   offenderSearchClient = OffenderSearchClient.prototype as jest.Mocked<OffenderSearchClient>
-  nonAssociationApi = NonAssociationsApi.prototype as jest.Mocked<NonAssociationsApi>
+  nonAssociationsApi = NonAssociationsApi.prototype as jest.Mocked<NonAssociationsApi>
 })
 
 afterEach(() => {
@@ -44,7 +49,7 @@ describe('Update non-association page', () => {
       stack: 'Not Found',
     }
     offenderSearchClient.getPrisoner.mockRejectedValue(error)
-    nonAssociationApi.getNonAssociation.mockResolvedValueOnce(nonAssociation)
+    nonAssociationsApi.getNonAssociation.mockResolvedValueOnce(nonAssociation)
 
     return request(app)
       .get(routeUrls.update(prisonerNumber, nonAssociationId))
@@ -62,26 +67,21 @@ describe('Update non-association page', () => {
       message: 'Not Found',
       stack: 'Not Found',
     }
-    nonAssociationApi.getNonAssociation.mockRejectedValue(error)
+    nonAssociationsApi.getNonAssociation.mockRejectedValue(error)
 
     return request(app)
       .get(routeUrls.update(prisonerNumber, nonAssociationId))
       .expect(404)
       .expect(res => {
         expect(res.text).not.toContain('Jones, David')
-        expect(nonAssociationApi.getNonAssociation).toHaveBeenCalledTimes(1)
+        expect(nonAssociationsApi.getNonAssociation).toHaveBeenCalledTimes(1)
       })
   })
 
   it('should render breadcrumbs', () => {
-    nonAssociationApi.getNonAssociation.mockResolvedValueOnce(nonAssociation)
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    offenderSearchClient.getPrisoner.mockImplementation(prisonerNumber => {
-      if (prisonerNumber === davidJones.prisonerNumber) {
-        return Promise.resolve(davidJones)
-      }
-      return Promise.resolve(fredMills)
-    })
+    nonAssociationsApi.getNonAssociation.mockResolvedValueOnce(nonAssociation)
+    offenderSearchClient.getPrisoner.mockResolvedValueOnce(prisoner)
+    offenderSearchClient.getPrisoner.mockResolvedValueOnce(otherPrisoner)
 
     return request(app)
       .get(routeUrls.update(prisonerNumber, nonAssociationId))
@@ -91,5 +91,46 @@ describe('Update non-association page', () => {
         expect(res.text).toContain('Jones, David')
         expect(offenderSearchClient.getPrisoner).toHaveBeenCalledTimes(2)
       })
+  })
+
+  describe('rendering a form before anything was submitted', () => {
+    describe.each([
+      ['via 1st prisoner', [prisoner, otherPrisoner]],
+      ['via 2nd prisoner', [otherPrisoner, prisoner]],
+    ])('when opening form %s', (scenario: string, prisonersOrder: Array<OffenderSearchResult>) => {
+      it('loads data in correct order', () => {
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        const [prisoner, otherPrisoner] = prisonersOrder
+        nonAssociationsApi.getNonAssociation.mockResolvedValueOnce(nonAssociation)
+
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        offenderSearchClient.getPrisoner.mockImplementation(prisonerNumber => {
+          if (prisonerNumber === davidJones.prisonerNumber) {
+            return Promise.resolve(davidJones)
+          }
+          return Promise.resolve(fredMills)
+        })
+
+        return request(app)
+          .get(routeUrls.update(prisoner.prisonerNumber, nonAssociation.id))
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .expect(res => {
+            expect(nonAssociationsApi.getNonAssociation).toHaveBeenCalledTimes(1)
+            expect(offenderSearchClient.getPrisoner).toHaveBeenCalledTimes(2)
+            expect(nonAssociationsApi.updateNonAssociation).not.toHaveBeenCalled()
+
+            expect(res.text).not.toContain('There is a problem')
+            expect(res.text).toContain(nonAssociation.comment)
+
+            // Check names are shown in correct order (key prisoner first)
+            const roles = res.text.match(/Is (.+) a victim or perpetrator\?/g)
+            expect(roles).toEqual([
+              `Is ${nameOfPerson(prisoner)} a victim or perpetrator?`,
+              `Is ${nameOfPerson(otherPrisoner)} a victim or perpetrator?`,
+            ])
+          })
+      })
+    })
   })
 })
