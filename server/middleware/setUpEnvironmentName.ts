efb1c9500a10540d7f36fa891d8633d86a919892
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-09-07 12:35:31 +0100

    Reuse already-existing template variable to allow fallback header to display the environment tag

--- server/middleware/setUpEnvironmentName.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-param-reassign */
-import express from 'express'
-import config from '../config'
-
-export default (app: express.Express) => {
-  app.locals.environmentName = config.environment
-  app.locals.environmentNameColour = config.environment === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
-}
