5a6ecf7029f809ea25a718f2977242ee5ebc2b3a
ushkarev <ushkarev@users.noreply.github.com>
2024-02-27 14:37:08 +0000

    NON-318: Sync changes from upstream template (#274)
    Fix manage details link to allow user to return (hmpps-template-typescript#307)

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -28,15 +28,20 @@ export default function setUpAuth(): Router {
   )
 
   const authUrl = config.apis.hmppsAuth.externalUrl
-  const authSignOutUrl = `${authUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
+  const authParameters = `client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
 
   router.use('/sign-out', (req, res, next) => {
+    const authSignOutUrl = `${authUrl}/sign-out?${authParameters}`
     if (req.user) {
       req.logout(err => {
         if (err) return next(err)
         return req.session.destroy(() => res.redirect(authSignOutUrl))
       })
     } else res.redirect(authSignOutUrl)
+  })
+
+  router.use('/account-details', (req, res) => {
+    res.redirect(`${authUrl}/account-details?${authParameters}`)
   })
 
   router.use((req, res, next) => {
