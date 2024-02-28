7e59a28a2fa5fee4dec19ff4f5df70b03e9f36eb
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-02-28 16:41:50 +0000

    Use stricter type-checking in integration tests

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -5,7 +5,8 @@ const wiremockAdminUrl = 'http://localhost:9091/__admin'
 export const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
   superagent.post(`${wiremockAdminUrl}/mappings`).send(mapping)
 
-export const getMatchingRequests = body => superagent.post(`${wiremockAdminUrl}/requests/find`).send(body)
+export const getMatchingRequests = (body: string | object) =>
+  superagent.post(`${wiremockAdminUrl}/requests/find`).send(body)
 
 export const resetStubs = (): Promise<Array<Response>> =>
   Promise.all([superagent.delete(`${wiremockAdminUrl}/mappings`), superagent.delete(`${wiremockAdminUrl}/requests`)])
